e55f19410f413be428d03416c867b7ef7d0a6d24
mohamed amine jaouadi <aminnejawadi@gmail.com>
2020-11-03 09:06:20 +0100

    last

--- src/server.js
+++
@@ -1,9 +0,0 @@
-
-app.use(express.static('./dist/live-resume.json'));
-
-app.get('/*',function (req,res){
-  res.sendfile('index.html',{root:'dist/live-resume.json/'});
-
-
-});
-app.listen(process.env.PORT || 8080);
